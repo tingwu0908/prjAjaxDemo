6b063d79d1d198bd4696c84365e88af7f0986626
TingWu <tingwu0908@gmail.com>
2023-08-07 14:19:50 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prjAjaxDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
