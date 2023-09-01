8dd12be6f4531b54a21cfd0c3ec33496c2ef7e60
TingWu <tingwu0908@gmail.com>
2023-09-01 17:00:11 +0800

    debug

---
+++ Controllers/AzController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjAjaxDemo.Controllers
+{
+    public class AzController : Controller
+    {
+        public IActionResult Index()
+        {
+            //    https://westus.dev.cognitive.microsoft.com/docs/services
+            return View();
+        }
+
+        public IActionResult VisionUpload()
+        {
+            return View();
+        }
+        public IActionResult ContentModerator()
+        {
+            return View();
+        }
+        public IActionResult TextModerator()
+        {
+            return View();
+        }
+    }
+}
